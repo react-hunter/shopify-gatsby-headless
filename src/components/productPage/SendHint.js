448d4118076776fd1c0014ea31042f9adad41f82
foxymen9 <foxymen9@gmail.com>
2021-06-14 15:47:11 +0700

    Remove something

--- src/components/productPage/SendHint.js
+++ src/components/productPage/SendHint.js
@@ -39,54 +39,51 @@ const sendEmail=(e)=>{
 
     const disableSendhint = () => {
         if (_isEmpty(yourName) || _isEmpty(recipientName) || _isEmpty(recipientEmail)) {
-            return true
+            return true;
         } else {
-            return false
+            return false;
         }
     }
 
   return (
-      <>
-          <div className="variantoverlayNew" >
-              <div className="send-hint__content">
-                  <span className="fa fa-times" size="1x" onClick={() => { props.onClose() }} />
-                  <header className="send-hint__header">
-                      <h1>{!isSuccess ? 'Send a Hint' : 'YOUR HINT HAS BEEN SENT!'}</h1>
-                      {!isSuccess && <p>They'll appreciate it, and you'll get what you want.</p>}
-                  </header>
-                  {!isSuccess && <div className="send-hint__main">
-                      <form>
-                          <div className="send-hint__fields">
-                              <div className="send-hint__field">
-                                  <label >Your Name</label>
-                                  <input className="" type="text" placeholder="Your Name" onChange={(e) => { setYourName(e.target.value) }} required />
-                              </div>
-
-                              <div className="send-hint__field">
-                                  <label >Your Email <span>(optional)</span></label>
-                                  <input className="" type="email" placeholder="Your Email" onChange={(e) => { setYourEmail(e.target.value) }} />
-                              </div>
-
-                              <div className=" send-hint__field">
-                                  <label >Recipient's Name</label>
-                                  <input className="" type="text" placeholder="Recipient's Name" required onChange={(e) => { setRecipientName(e.target.value) }} />
-                              </div>
-
-                              <div className=" send-hint__field">
-                                  <label>Recipient's Email</label>
-                                  <input className="" type="email" placeholder="Recipient's Email" required onChange={(e) => { setRecipientEmail(e.target.value) }} />
-                              </div>
+      <div className="variantoverlayNew" >
+          <div className="send-hint__content">
+              <span className="fa fa-times" size="1x" onClick={() => { props.onClose() }} />
+              <header className="send-hint__header">
+                  <h1>{!isSuccess ? 'Send a Hint' : 'YOUR HINT HAS BEEN SENT!'}</h1>
+                  {!isSuccess && <p>They'll appreciate it, and you'll get what you want.</p>}
+              </header>
+              {!isSuccess && <div className="send-hint__main">
+                  <form>
+                      <div className="send-hint__fields">
+                          <div className="send-hint__field">
+                              <label >Your Name</label>
+                              <input type="text" placeholder="Your Name" onChange={(e) => { setYourName(e.target.value) }} required />
                           </div>
-                          <div >
-                              <button type="submit" disabled={disableSendhint()} className="send-hint__submit " onClick={(e)=>sendEmail(e)}>Send Hint</button>
+
+                          <div className="send-hint__field">
+                              <label >Your Email <span>(optional)</span></label>
+                              <input type="email" placeholder="Your Email" onChange={(e) => { setYourEmail(e.target.value) }} />
                           </div>
-                      </form>
-                  </div>
-                  }
+
+                          <div className="send-hint__field">
+                              <label >Recipient's Name</label>
+                              <input type="text" placeholder="Recipient's Name" required onChange={(e) => { setRecipientName(e.target.value) }} />
+                          </div>
+
+                          <div className="send-hint__field">
+                              <label>Recipient's Email</label>
+                              <input type="email" placeholder="Recipient's Email" required onChange={(e) => { setRecipientEmail(e.target.value) }} />
+                          </div>
+                      </div>
+                      <div >
+                          <button disabled={disableSendhint()} className="send-hint__submit " onClick={(e) => sendEmail(e)}>Send Hint</button>
+                      </div>
+                  </form>
               </div>
+              }
           </div>
-
-      </>
+      </div>
   );
 };
 
