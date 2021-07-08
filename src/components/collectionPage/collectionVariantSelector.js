a41957afbd38d9bc940f6859c43a3b5c0ad7aed1
foxymen9 <foxymen9@gmail.com>
2021-07-08 22:32:39 +0700

    Fix issue full screen

--- src/components/collectionPage/collectionVariantSelector.js
+++ src/components/collectionPage/collectionVariantSelector.js
@@ -269,15 +269,15 @@ const CollectionVariantSelector = React.memo(function CollectionVariantSelector(
 		}
 	}
 	const isFullImage =()=>{
-		return (mainOption.name !== 'Title' && mainOption.name !== 'Quantity' && product.productType !== 'Enchanted Rose'
-		&& product.productType !== 'Rose Bear' && product.productType !== 'Luxury Robe');
+		return !(mainOption.name !== 'Title' && mainOption.name !== 'Quantity' && product.productType !== 'Enchanted Rose'
+		&& product.productType !== 'Rose Bear' && product.productType !== 'Luxury Robe' && product.productType !== 'Bikini');
 	}
   
 	return (
 		<>
 		<div className="variantoverlayNew" id="variantOverlay-">
 			<div className="variantSelector_wrapper animate-bottom" data-toggle="modal">
-				<div className={isFullImage() ? "variantSelector-section" : "variantSelector-section-full variantSelector-section"}> 
+				<div className={!isFullImage() ? "variantSelector-section" : "variantSelector-section-full variantSelector-section"}> 
 					<div className="closeVariantSelector">
 						<div className="closeVariantSelector_content">
 							<span className="variantSelector_close_message"
@@ -289,21 +289,21 @@ const CollectionVariantSelector = React.memo(function CollectionVariantSelector(
 						</div>
 						<div className={product.productType !== 'Enchanted Rose' ? "closeVariantSelector-mobile_swipe" : "closeVariantSelector-mobile_swipe-white"}></div>
 					</div>
-					<div className={isFullImage() ?"preview-main-option_wrapper" : "preview-main-option_wrapper-full preview-main-option_wrapper"}>
+					<div className={!isFullImage() ?"preview-main-option_wrapper" : "preview-main-option_wrapper-full preview-main-option_wrapper"}>
 						{ 
 						product.productType !== 'Lingerie'? 
-							<div className={isFullImage() ? "preview_wrapper": "preview_wrapper-large preview_wrapper"}>
+							<div className={!isFullImage() ? "preview_wrapper": "preview_wrapper-large preview_wrapper"}>
 								{/* { variant.image &&
 									<GatsbyImage image={variant.image.imageData ? variant.image.imageData.childImageSharp.gatsbyImageData : props.placeholderImage} 
 										className="variantSelector-preview_img"
 										loading="lazy" alt={variant.title} />
 								} */}
-								{ variant.image && isFullImage() &&
+								{ variant.image && !isFullImage() &&
 									<LazyLoadImage src={variant.image.originalSrc}
 										className={mainOption.name !== 'Title' ? "variantSelector-preview_img" : "variantSelector-preview-large_img"}
 										effect="blur" loading="eager" alt={variant.title} />
 								}
-								{!isFullImage() && <ProductGallery product={getProduct(product)} isVarantSelected={true} selectedVariant={variant} key="product-gallery" hidden={true} />}
+								{isFullImage() && <ProductGallery product={getProduct(product)} isVarantSelected={true} selectedVariant={variant} key="product-gallery" hidden={true} />}
 							</div>
 						: 
 						<div className="preview_wrapper special_ratio">
